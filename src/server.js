f876626a820d3d6b95e9c118cfac6d5f21b35961
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-07-05 16:23:24 -0300

    ajustes express

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { resolve } = require('path');
+
+const app = express();
+
+app.use('/', express.static(
+    resolve(__dirname, './build')
+))
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) return console.error(err);
+    else console.log('Tudo OK!')
+})
